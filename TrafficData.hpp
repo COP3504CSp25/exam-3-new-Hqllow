9fb32d53c4c09ddd76ccdb83d78900d38635dcc5
Bradley Cederholm <106129785+Hqllow@users.noreply.github.com>
2025-04-17 12:46:13 +0000

    add question 4

--- TrafficData.hpp
+++ TrafficData.hpp
@@ -6,10 +6,10 @@
 #include <string>
 #include <sstream>
 
+
+//I don't remember code for how to get input from a csv file, so I cannot solve this question and have left it blank.
 std::map<std::string, int> loadTrafficData(const std::string& filename) {
-  //implement your function here!
 }
 
 void updateTrafficData(const std::string& filename, std::map<std::string, int>& dataMap) {
-    //implement your function here!
 }
