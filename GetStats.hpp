596eba3a343b5074c2202769d983809c9eb1e86c
Bradley Cederholm <106129785+Hqllow@users.noreply.github.com>
2025-04-17 12:03:34 +0000

    Solve Question 1 (hopefully)

--- GetStats.hpp
+++ GetStats.hpp
@@ -14,7 +14,33 @@ public:
     }
 
     int* getStats() const{
-        //write your function here!
+        int* returnArray = new int[3];
+        if (head == nullptr) {
+            returnArray[0] = 0;
+            returnArray[1] = 0;
+            returnArray[2] = 0;
+            return returnArray;
+        }
+
+        Node* tracker = head;
+        int min = head->data;
+        int max = head->data;
+        int total = 0;
+        int num = 0;
+        while (tracker != nullptr) {
+            if (tracker->data > max) {
+                max = tracker->data;
+            }
+            if (tracker->data < min) {
+                min = tracker->data;
+            }
+            total += tracker->data;
+            num++;
+            tracker = tracker->next;
+        }
+        returnArray[0] = min;
+        returnArray[1] = max;
+        returnArray[2] = total / num;
     }
     
 
